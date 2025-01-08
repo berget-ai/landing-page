8800d740910dbff3464e2053d46fc1ad2af467cd
Christian Landgren <christian@landgren.nu>
2025-01-08 23:53:25 +0100

    refactor: Remove redundant flag component definitions in LanguageSwitcher

--- src/components/ui/LanguageSwitcher.tsx
+++ src/components/ui/LanguageSwitcher.tsx
@@ -1,13 +1,6 @@
 import { useTranslation } from 'react-i18next'
 import { FlagEU, FlagSE } from '@weston/react-world-flags'
 
-interface FlagProps extends React.SVGProps<SVGSVGElement> {
-  className?: string
-}
-
-const FlagSE: React.FC<FlagProps> = (props) => <svg {...props} />
-const FlagEU: React.FC<FlagProps> = (props) => <svg {...props} />
-
 export function LanguageSwitcher() {
   const { i18n } = useTranslation()
 
@@ -15,11 +8,11 @@ export function LanguageSwitcher() {
     <div className="flex gap-2 aspect-square">
       {i18n.language === 'en' ? (
         <button onClick={() => i18n.changeLanguage('sv')} className="gap-2  ">
-          <FlagSE className="w-6" />
+          <FlagSE />
         </button>
       ) : (
         <button onClick={() => i18n.changeLanguage('en')} className="gap-2 ">
-          <FlagEU className="w-6" />
+          <FlagEU />
         </button>
       )}
     </div>
