a696da1be35e8b8885498be122cbbde7d594996f
i97andlu <i97andlu@gmail.com>
2025-06-03 18:26:34 +0200

    logos II

--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -101,7 +101,7 @@ export default function PartnersPage() {
       <div className="container mx-auto px-4 py-12">
         <div className="max-w-4xl mx-auto mb-16">
           <motion.div
-            initial={{ opacity: 0, y: 20 }}
+            initial={{ opacity: 0, y: 80 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
           >
